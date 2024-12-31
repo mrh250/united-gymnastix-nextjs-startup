7f5d1f0eac9043ca2864151307c3a8e9438e98af
Matt <m.russell.halstead@gmail.com>
2024-12-30 22:50:12 -0500

    Working on formatting.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-black sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   Free and Open-Source Next.js Template for Startup & SaaS
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
